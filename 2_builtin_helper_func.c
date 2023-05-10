32d22fdbd71a342e197c38c19e877f0b13995cb0
elvisndungu <senkutoshi@gmail.com>
2023-05-10 10:31:11 +0200

    New commit to Shell

---
+++ 2_builtin_helper_func.c
@@ -0,0 +1,29 @@
+#include "shell.h"
+/**
+ * builtins_list - search for match and execute the associate builtin
+ * @data: struct for the program's data
+ * Return: result on match
+ **/
+int builtins_list(data_of_program *data)
+{
+	int iterator;
+	builtins options[] = {
+		{"exit", builtin_exit},
+		{"help", builtin_help},
+		{"cd", builtin_cd},
+		{"alias", builtin_alias},
+		{"env", builtin_env},
+		{"setenv", builtin_set_env},
+		{"unsetenv", builtin_unset_env},
+		{NULL, NULL}
+	};
+	
+	for (iterator = 0; options[iterator].builtin != NULL; iterator++)
+	{
+		if (str_compare(options[iterator].builtin, data->command_name, 0))
+		{
+			return (options[iterator].function(data));
+		}
+	}
+	return (-1);
+}
