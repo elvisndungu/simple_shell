553571a4f92d14bbdd3d9ad0075027e53801238e
elvisndungu <senkutoshi@gmail.com>
2023-05-16 11:18:44 +0200

    New commit to Shell

---
+++ 3_miscellaneous.c
@@ -0,0 +1,117 @@
+#include "shell.h"
+
+int check_file(char *full_path);
+
+/**
+ * find_program - find a program in path
+ * @data: a pointer to the program's data
+ * Return: 0 or err code N_N
+ */
+int find_program(data_of_program *data)
+{
+	int i = 0, ret_code = 0;
+	char **directories;
+	
+	if (!data->command_name)
+		return (2);
+	
+	if (data->command_name[0] == '/' || data->command_name[0] == '.')
+		return (check_file(data->command_name));
+	
+	free(data->tokens[0]);
+	data->tokens[0] = str_concat(str_duplicate("/"), data->command_name);
+	if (!data->tokens[0])
+		return (2);
+	
+	directories = tokenize_path(data);/* search in the PATH */
+	
+	if (!directories || !directories[0])
+	{
+		errno = 127;
+		return (127);
+	}
+	for (i = 0; directories[i]; i++)
+	{
+		directories[i] = str_concat(directories[i], data->tokens[0]);
+		ret_code = check_file(directories[i]);
+		if (ret_code == 0 || ret_code == 126)
+		{
+			errno = 0;
+			free(data->tokens[0]);
+			data->tokens[0] = str_duplicate(directories[i]);
+			free_array_of_pointers(directories);
+			return (ret_code);
+		}
+	}
+	free(data->tokens[0]);
+	data->tokens[0] = NULL;
+	free_array_of_pointers(directories);
+	return (ret_code);
+}
+
+/**
+ * tokenize_path - tokenize the path in dirs
+ * @data: a pointer to the program's data
+ * Return: arr of path dirs
+ */
+char **tokenize_path(data_of_program *data)
+{
+	int i = 0;
+	int counter_directories = 2;
+	char **tokens = NULL;
+	char *PATH;
+	
+	/* get the PATH value*/
+	PATH = env_get_key("PATH", data);
+	if ((PATH == NULL) || PATH[0] == '\0')
+	{/*path not found*/
+		return (NULL);
+	}
+	
+	PATH = str_duplicate(PATH);
+	
+	/* find the number of directories in the PATH */
+	for (i = 0; PATH[i]; i++)
+	{
+		if (PATH[i] == ':')
+			counter_directories++;
+	}
+	
+	/* reserve space for the array of pointers */
+	tokens = malloc(sizeof(char *) * counter_directories);
+	
+	/*tokenize and duplicate each token of path*/
+	i = 0;
+	tokens[i] = str_duplicate(_strtok(PATH, ":"));
+	while (tokens[i++])
+	{
+		tokens[i] = str_duplicate(_strtok(NULL, ":"));
+	}
+	
+	free(PATH);
+	PATH = NULL;
+	return (tokens);
+	
+}
+
+/**
+ * check_file - checks if file exists and wit right perms
+ * @full_path: pointer to the full file name
+ * Return: num
+ */
+int check_file(char *full_path)
+{
+	struct stat sb;
+	
+	if (stat(full_path, &sb) != -1)
+	{
+		if (S_ISDIR(sb.st_mode) ||  access(full_path, X_OK))
+		{
+			errno = 126;
+			return (126);
+		}
+		return (0);
+	}
+	errno = 127;
+	return (127);
+}
